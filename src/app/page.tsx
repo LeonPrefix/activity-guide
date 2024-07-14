360adcce6be543bc4ab4decf767534211341395f
LeonPrefix <herrprefix@gmail.com>
2024-07-14 19:03:42 +0200

    search page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,75 @@
-export default function Home() {
-  return <>test</>;
+"use client";
+
+import Map from "@/components/map";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Slider } from "@/components/ui/slider";
+import { cn } from "@/lib/utils";
+import { CalendarIcon, DoubleArrowDownIcon, RocketIcon } from "@radix-ui/react-icons";
+import { addDays, format } from "date-fns";
+import { useState } from "react";
+import { DateRange } from "react-day-picker";
+
+const DEFAULT_CENTER = { lat: 53.5488, lng: 9.9872 };
+
+export default function Search() {
+  const [radius, setRadius] = useState(500);
+  const [marker, setMarker] = useState<google.maps.LatLngLiteral>(DEFAULT_CENTER);
+
+  const [date, setDate] = useState<DateRange | undefined>({ from: new Date(), to: addDays(new Date(), 7) });
+
+  return (
+    <main className="flex flex-col items-center p-8">
+      <div className="flex flex-col items-center gap-2 text-center">
+        <span className="font-mono text-6xl tracking-widest font-semibold">Activity Guide</span>
+        <span className="text-muted-foreground text-2xl font-light">
+          Plane einen Ausflug anhand aktueller Wetter- und Ortsdaten
+        </span>
+      </div>
+      <DoubleArrowDownIcon className="m-12 w-8 h-8" />
+      <div className="w-full xl:w-[70%] h-[65vh] flex flex-col justify-center gap-2">
+        <div className="flex flex-col gap-2 lg:block">
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "w-full lg:w-[280px] justify-start text-left font-normal",
+                  (!date?.from || !date?.to) && "text-muted-foreground"
+                )}
+              >
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                {date?.from && date?.to ? (
+                  <>
+                    {format(date.from, "dd.MM.yy")} - {format(date.to, "dd.MM.yy")}
+                  </>
+                ) : (
+                  <span className="animate-pulse">Wähle einen Zeitraum</span>
+                )}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[280px] p-0" align="start">
+              <Calendar
+                initialFocus
+                mode="range"
+                defaultMonth={date?.from}
+                selected={date}
+                onSelect={setDate}
+                fromDate={new Date()}
+                toDate={addDays(new Date(), 7)}
+              />
+            </PopoverContent>
+          </Popover>
+          <span className="float-right flex h-full items-center lg:w-[50%] w-full gap-4">
+            Radius: <Slider value={[radius]} onValueChange={(v) => setRadius(v[0])} min={300} step={100} max={3000} />
+          </span>
+        </div>
+        <Map radius={radius} marker={marker} setMarker={setMarker} />
+        <Button className="bg-green-600 hover:bg-green-700" disabled={!date?.from || !date?.to}>
+          <RocketIcon className="mr-2 h-4 w-4" /> Suche nach den perfekten Aktivitäten!
+        </Button>
+      </div>
+    </main>
+  );
 }
---
+++ src/components/map.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { CircleF, GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
+import { useState } from "react";
+
+interface MapProps {
+  radius: number;
+  marker: google.maps.LatLngLiteral;
+  setMarker: (latLng: google.maps.LatLngLiteral) => void;
+}
+
+export default function Map({ radius, marker, setMarker }: MapProps) {
+  const { isLoaded, loadError } = useJsApiLoader({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API as string,
+    libraries: ["geometry"],
+  });
+
+  const [mapRef, setMapRef] = useState<google.maps.Map>();
+  const [center, setCenter] = useState<google.maps.LatLngLiteral>({ ...marker });
+
+  if (loadError)
+    return (
+      <div className="bg-muted rounded w-full h-full flex justify-center items-center text-xl font-semibold font-mono">
+        Es ist ein Fehler aufgetreten: {loadError.message}
+      </div>
+    );
+  if (!isLoaded)
+    return (
+      <div className="bg-muted rounded w-full h-full flex justify-center items-center text-xl font-semibold font-mono animate-pulse">
+        Lade Karte...
+      </div>
+    );
+
+  return (
+    <GoogleMap
+      mapContainerStyle={{ width: "100%", height: "100%", borderRadius: "0.25rem" }}
+      zoom={15}
+      onLoad={(map) => setMapRef(map)}
+      onClick={(e) => {
+        if (!mapRef) return;
+
+        const newCenter = mapRef.getCenter();
+        if (newCenter) setCenter({ lat: newCenter?.lat(), lng: newCenter?.lng() });
+        setMarker({ lat: e.latLng?.lat() || marker.lat, lng: e.latLng?.lng() || marker.lng });
+      }}
+      options={{
+        tilt: 0,
+        center,
+        mapTypeId: "roadmap",
+        mapTypeControl: false,
+        fullscreenControl: false,
+        keyboardShortcuts: false,
+        streetViewControl: false,
+        styles: [
+          { elementType: "labels", featureType: "poi", stylers: [{ visibility: "off" }] },
+          { elementType: "labels", featureType: "transit", stylers: [{ visibility: "off" }] },
+        ],
+      }}
+    >
+      <MarkerF
+        position={marker}
+        draggable
+        onDrag={(e) => setMarker({ lat: e.latLng?.lat() || marker.lat, lng: e.latLng?.lng() || marker.lng })}
+      />
+      <CircleF center={marker} radius={radius} options={{ fillOpacity: 0.1 }} />
+    </GoogleMap>
+  );
+}
